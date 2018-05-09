379133243fe670cff6bee69dfe2648756740dd83
Enrique <enriquelozada95@hotmail.com>
2018-05-09 00:11:27 -0500

    tests front

--- test/specs/ContentHeader.spec.js
+++ test/specs/ContentHeader.spec.js
@@ -5,12 +5,17 @@ import ContentHeader from '../../components/ContentHeader'
 let wrapper
 
 const title = "new title"
+const sdbar = "new sdbar"
 
 test.beforeEach(() => {
   wrapper = mount(ContentHeader, {
     propsData: { title },
   })
 })
+
+test('test test', t => {
+	t.pass();
+});
 
 test('ContentHeader.vue shapshow', (t) => {
   t.snapshot({ html: wrapper.html() })
